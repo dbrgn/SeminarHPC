327fbdf721256b6aa14f693e3fff76dace3c427f
Andreas Mueller <andreas.mueller@hsr.ch>
2014-02-17 10:31:38 +0100

    flush results immediately

--- code/gauss/pthread/results.R
+++ code/gauss/pthread/results.R
@@ -7,12 +7,11 @@ u$ln = log10(u$n)
 u$lt = log10(u$time)
 
 pdf("results.pdf", 8, 6)
-plot(d[d$threads == 16,]$ln, d[d$threads == 16,]$lt, type = "l",
+plot(d[d$threads == 32,]$ln, d[d$threads == 32,]$lt, type = "l",
 	main = "Laufzeit Gauss Pthread-Implementation",
 	xlab = "log10(n)", ylab = "log10(Laufzeit)" )
-lines(d[d$threads == 14,]$ln, d[d$threads == 14,]$lt)
+lines(d[d$threads == 16,]$ln, d[d$threads == 16,]$lt)
 lines(d[d$threads == 12,]$ln, d[d$threads == 12,]$lt)
-lines(d[d$threads == 10,]$ln, d[d$threads == 10,]$lt)
 lines(d[d$threads == 8,]$ln, d[d$threads == 8,]$lt)
 lines(d[d$threads == 4,]$ln, d[d$threads == 4,]$lt)
 lines(d[d$threads == 2,]$ln, d[d$threads == 2,]$lt)
