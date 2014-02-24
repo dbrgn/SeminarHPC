f371d8416693f5cf3c715744cd378376b134f57e
Andreas Mueller <andreas.mueller@hsr.ch>
2014-02-24 13:43:51 +0100

    minor fixes

--- skript/sequentialforward.c
+++ skript/sequentialforward.c
@@ -1,8 +1,10 @@
 for (int i = 0; i < n; i++) {
         float   pivot = M(a, 2 * n, i, i);
+        // Pivot-Zeile dividieren
         for (int j = minj + 1; j < 2 * n; j++) {
                 M(a, 2 * n, i, j) /= pivot;
         }
+        // Zeilenoperationen
         for (int k = i + 1; k < n; k++) {
                 float   b = M(a, 2 * n, k, i);
                 for (int j = minj + 1; j < 2 * n; j++) {
