a5a66aa12edd16934b6f232168156fd56350ceab
Andreas Mueller <andreas.mueller@hsr.ch>
2014-03-13 17:33:21 +0100

    Julai-Mengen Implementation hinzugefügt

---
+++ code/julia/julia.c
@@ -0,0 +1,494 @@
+/*
+ * gauss.c -- opencl implementation of Gauss algorithm
+ *
+ * (c) 2014 Prof Dr Andreas Mueller, Hochschule Rapperswil
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <CL/opencl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <alloca.h>
+#include <getopt.h>
+
+int	debug = 0;
+
+/*
+ * \brief Auxiliary function to read OpenCL code from a file
+ *
+ * This function reads the contents of a file and invokes the
+ * clCreateProgramWithSource to create a program.
+ */
+cl_program	cluCreateProgramWithFile(cl_context context,
+			const char *filename, cl_int *err) {
+	// first check that the file exists, but using stat will also give
+	// the file size which can later be used to size a buffer
+	struct stat	sb;
+	if (stat(filename, &sb) < 0) {
+		*err = -1;
+		return NULL;
+	}
+
+	// open the file for reading
+	int	fd = open(filename, O_RDONLY);
+	if (fd < 0) {
+		*err = -1;
+		return NULL;
+	}
+
+	// read the file data into a memory buffer, allocated on the stack
+	// from the file size info in the stat structure received previously
+	size_t	length[1];
+	length[0] = sb.st_size;
+	char	*source[1];
+	source[0] = alloca(sb.st_size);
+	if (sb.st_size != read(fd, source[0], sb.st_size)) {
+		*err = -1;
+		return NULL;
+	}
+	close(fd);
+
+	// use the clCreateProgramWithSource function to create the program
+	cl_program	program;
+	program = clCreateProgramWithSource(context, 1, (const char **)source,
+		length, err);
+
+	return program;
+}
+
+/**
+ * \brief Main function
+ */
+int	main(int argc, char *argv[]) {
+	// parse command line arguments
+	int	gpu = 0;	// whether to use the GPU or the CPU
+	int	c;
+	int	platform = 0;	// platform number
+	int	Debug = 0;
+	int	height = 128;
+	int	width = 128;
+	double	originx = -1;
+	double	originy = -1;
+	double	sizex = 2;
+	double	sizey = 2;
+	int	sx = 128;
+	int	sy = 128;
+	double	cx = 0;
+	double	cy = 0;
+	while (EOF != (c = getopt(argc, argv, "gdP:Dv:w:h:x:y:W:H:s:t:u:v:")))
+		switch (c) {
+		case 'd':
+			debug = 1;
+			break;
+		case 'D':
+			Debug = 1;
+			break;
+		case 'g':
+			gpu = 1;
+			break;
+		case 'P':
+			platform = atoi(optarg);
+			break;
+		case 'w':
+			width = atoi(optarg);
+			break;
+		case 'h':
+			height = atoi(optarg);
+			break;
+		case 'x':
+			originx = atof(optarg);
+			break;
+		case 'y':
+			originy = atof(optarg);
+			break;
+		case 'W':
+			sizex = atof(optarg);
+			break;
+		case 'H':
+			sizey = atof(optarg);
+			break;
+		case 's':
+			sx = atoi(optarg);
+			break;
+		case 't':
+			sy = atoi(optarg);
+			break;
+		case 'u':
+			cx = atof(optarg);
+			break;
+		case 'v':
+			cy = atof(optarg);
+			break;
+		}
+
+	// start OpenCL initialization
+	int	err;
+
+	// first get the platform info
+	cl_uint	num_platforms;
+	err = clGetPlatformIDs(0, NULL, &num_platforms);
+	if (err != CL_SUCCESS) {
+		fprintf(stderr, "cannot get the number of platforms\n");
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "%s:%d: found %d platforms\n",
+			__FILE__, __LINE__, num_platforms);
+	}
+
+	cl_platform_id	*platformIds = (cl_platform_id *)malloc(
+		num_platforms * sizeof(cl_platform_id));
+	err = clGetPlatformIDs(num_platforms, platformIds, 0);
+
+	// display information about the platforms
+	typedef enum {
+		PLATFORM_AMD, PLATFORM_NVIDIA, PLATFORM_INTEL
+	} platformcode;
+	platformcode	*codes = (platformcode *)malloc(sizeof(platformcode)
+					* num_platforms);
+	if (debug) {
+		for (int i = 0; i < num_platforms; i++) {
+			size_t size;
+
+			// platform name
+			cl_platform_id	id = platformIds[i];
+			err = clGetPlatformInfo(id, CL_PLATFORM_NAME, 0, NULL,
+				&size);
+			char * name = (char *)alloca(sizeof(char) * size);
+			err = clGetPlatformInfo(id, CL_PLATFORM_NAME, size,
+				name, NULL);
+
+			if (0 == strncmp(name, "NVIDIA", 6)) {
+				codes[i] = PLATFORM_NVIDIA;
+			}
+			if (0 == strncmp(name, "AMD", 3)) {
+				codes[i] = PLATFORM_AMD;
+			}
+			if (0 == strncmp(name, "Intel", 5)) {
+				codes[i] = PLATFORM_INTEL;
+			}
+
+			// platform vendor
+			err = clGetPlatformInfo(id, CL_PLATFORM_VENDOR, 0, NULL,
+				&size);
+			char * vname = (char *)alloca(sizeof(char) * size);
+			err = clGetPlatformInfo(id, CL_PLATFORM_VENDOR, size,
+				vname, NULL);
+
+			// platform version
+			err = clGetPlatformInfo(id, CL_PLATFORM_VERSION, 0,
+				NULL, &size);
+			char *version = (char *)alloca(sizeof(char) * size);
+			err = clGetPlatformInfo(id, CL_PLATFORM_VERSION, size,
+				version, NULL);
+			fprintf(stderr, "platform[%d]: %s/%s %s\n", i, name,
+				vname, version);
+
+			// platform extensions
+			err = clGetPlatformInfo(id, CL_PLATFORM_EXTENSIONS, 0,
+				NULL, &size);
+			char *extensions = (char *)alloca(sizeof(char) * size);
+			err = clGetPlatformInfo(id, CL_PLATFORM_EXTENSIONS,
+				size, extensions, NULL);
+			fprintf(stderr, "extensions[%d]: %s\n", i, extensions);
+		}
+	}
+
+	// if we are on the nvidia platform, then we don't allow the -D
+	// flag to the compiler, because the Nvidia platform does not support
+	// the printf extension that most other OpenCL implementations support.
+	if (PLATFORM_NVIDIA == codes[platform]) {
+		Debug = 0;
+		gpu = 1;
+	}
+
+	// get a compute device of the requested type (GPU/CPU)
+	cl_device_id	device_id;
+	err = clGetDeviceIDs(platformIds[platform],
+		gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device_id,
+		NULL);
+	if (err != CL_SUCCESS) {
+		fprintf(stderr, "%s:%d: no device id found: %d\n",
+			__FILE__, __LINE__, err);
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "%s:%d: got device id %p\n",
+			__FILE__, __LINE__, device_id);
+	}
+
+	// we want to find out some interesting device parameters, in particular
+	// we want to know how large memory, is, cache characteristics and
+	// simd width (preferred vector size)
+	cl_ulong	globalmemsize = 0, localmemsize = 0, cachesize = 0;
+	size_t	paramsize = sizeof(globalmemsize);
+	size_t	paramsizeret = 0;
+	clGetDeviceInfo(device_id, CL_DEVICE_GLOBAL_MEM_SIZE, 
+		paramsize, &globalmemsize, &paramsizeret);
+	clGetDeviceInfo(device_id, CL_DEVICE_LOCAL_MEM_SIZE, 
+		paramsize, &localmemsize, &paramsizeret);
+	clGetDeviceInfo(device_id, CL_DEVICE_GLOBAL_MEM_CACHE_SIZE, 
+		paramsize, &cachesize, &paramsizeret);
+
+	cl_uint	computeunits = 0;
+	paramsize = sizeof(computeunits);
+	clGetDeviceInfo(device_id, CL_DEVICE_MAX_COMPUTE_UNITS,
+		paramsize, &computeunits, &paramsizeret);
+
+	cl_uint cacheline = 0;
+	paramsize = sizeof(cacheline);
+	clGetDeviceInfo(device_id, CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE,
+		paramsize, &cacheline, &paramsizeret);
+
+	cl_uint	vectorwidth = 0;
+	paramsize = sizeof(vectorwidth);
+	clGetDeviceInfo(device_id, CL_DEVICE_PREFERRED_VECTOR_WIDTH_FLOAT,
+		paramsize, &vectorwidth, &paramsizeret);
+
+	cl_bool	unified = 0;
+	paramsize = sizeof(unified);
+	clGetDeviceInfo(device_id, CL_DEVICE_HOST_UNIFIED_MEMORY,
+		paramsize, &unified, &paramsizeret);
+
+	if (debug) {
+		fprintf(stderr, "unified memory:  %s\n",
+			(unified) ? "yes" : "no");
+		fprintf(stderr, "global memory:   %lu\n", globalmemsize);
+		fprintf(stderr, "local memory:    %lu\n", localmemsize);
+		fprintf(stderr, "cache size:      %lu\n", cachesize);
+		fprintf(stderr, "cache line size: %u\n", cacheline);
+		fprintf(stderr, "compute units:   %u\n", computeunits);
+		fprintf(stderr, "vector width:    %u\n", vectorwidth);
+	}
+
+	// create a context. A number of devices can be attached to a context,
+	// but in our case, there is only one
+	cl_context	context;
+	context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+	if (!context) {
+		fprintf(stderr, "%s:%d: cannot create context: %d\n",
+			__FILE__, __LINE__, err);
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "%s:%d: got context\n", __FILE__, __LINE__);
+	}
+
+	// create a command queue. Each device needs its own command queue
+	cl_command_queue	commands;
+	commands = clCreateCommandQueue(context, device_id, 0, &err);
+	if (!commands) {
+		fprintf(stderr, "%s:%d: cannot create command queue: %d\n",
+			__FILE__, __LINE__,  err);
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "%s:%d: got command queue\n",
+			__FILE__, __LINE__);
+	}
+
+	// create a program from a source file. There is no OpenCL function
+	// to read a program from a file, so we supply a uitility function
+	// for this purpose.
+	cl_program	program = cluCreateProgramWithFile(context,
+				"julia.cl", &err);
+	if (!program) {
+		fprintf(stderr, "%s:%d: cannot create program: %d\n",
+			__FILE__, __LINE__, err);
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "%s:%d: got program\n", __FILE__, __LINE__);
+	}
+
+	// compile a program. The compiler accepts flags, and we would like
+	// to use them to control some aspects of our implementation, in
+	// particular the use of vector primitives.
+	char	flags[200];
+	memset(flags, 0, sizeof(flags));
+	switch (codes[platform]) {
+	case PLATFORM_AMD:
+		strcpy(flags, "-DAMD");
+		break;
+	case PLATFORM_INTEL:
+		strcpy(flags, "-DINTEL");
+		break;
+	case PLATFORM_NVIDIA:
+		strcpy(flags, "-DNVIDIA");
+		break;
+	}
+	if (Debug) {
+		strcat(flags, " -DDEBUG");
+	}
+
+	err = clBuildProgram(program, 1, &device_id, flags, NULL, NULL);
+	if (err) {
+		fprintf(stderr, "%s:%d: cannot compile program: %d\n",
+			__FILE__, __LINE__, err);
+		size_t	l = 32 * 1024;
+		char	*log = (char *)malloc(l);
+		err = clGetProgramBuildInfo(program, device_id,
+			CL_PROGRAM_BUILD_LOG,
+			l, log, &l);
+		if (err) {
+			fprintf(stderr, "%s:%d: cannot retrieve log: %d\n",
+				__FILE__, __LINE__, err);
+			return EXIT_FAILURE;
+		}
+		fprintf(stderr, "compile log:\n%*s\n", (int)l, log);
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "%s:%d: compiled the program\n",
+			__FILE__, __LINE__);
+	}
+
+	// create a kernel. This step is necessary because an OpenCL program
+	// can contain many kernels. This function then provides a handle
+	// to call a particular kernel by its name.
+	cl_kernel	kernel = clCreateKernel(program, "iterate", &err);
+	if (!kernel) {
+		fprintf(stderr, "%s:%d: cannot create a kernel: %d\n",
+			__FILE__, __LINE__, err);
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "%s:%d: kernel created\n", __FILE__, __LINE__);
+	}
+
+	// get the local memory requirements of this kernel
+	cl_ulong	localmemreq = 0;
+	err = clGetKernelWorkGroupInfo(kernel, device_id,
+		CL_KERNEL_LOCAL_MEM_SIZE, sizeof(localmemreq), &localmemreq,
+		NULL);
+	if (debug) {
+		fprintf(stderr, "local mem req:   %lu\n", localmemreq);
+	}
+
+	// get workgroup info. When 
+	size_t	workgroupsize;
+	err = clGetKernelWorkGroupInfo(kernel, device_id,
+		CL_KERNEL_WORK_GROUP_SIZE,
+		sizeof(workgroupsize), &workgroupsize, NULL);
+	if (err != CL_SUCCESS) {
+		fprintf(stderr, "%s:%d: can't get kernel work group info: %d\n",
+			__FILE__, __LINE__, err);
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "work group size: %lu\n", workgroupsize);
+	}
+
+	// allocate the OpenCL memory buffers
+	cl_mem	parameters = NULL, output = NULL;
+
+	// we want to have to cluster points
+	int	npoints = 2;
+	int	psize = 7 + 2 * npoints;
+
+	// create parameter buffer
+	double	*p = (double *)malloc(sizeof(double) * psize);
+	parameters = clCreateBuffer(context, CL_MEM_READ_ONLY,
+		sizeof(double) * psize, NULL, NULL);
+	if (!parameters) {
+		fprintf(stderr, "%s:%d: cannot allocate parameter buffer\n",
+			__FILE__, __LINE__);
+		return EXIT_FAILURE;
+	}
+	p[0] = originx;
+	p[1] = originy;
+	p[2] = sizex / width;
+	p[3] = sizey / height;
+	p[4] = cx;
+	p[5] = cy;
+	p[6] = 2;
+	p[7] = 0;
+	p[8] = 0;
+	p[9] = 1291;
+	p[10] = 4711;
+
+	// create output buffer
+	output = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+		sizeof(int) * width * height, NULL, NULL);
+	if (!output) {
+		fprintf(stderr, "%s:%d: cannot allocate output buffer\n",
+			__FILE__, __LINE__);
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "%s:%d: buffers allocated\n",
+			__FILE__, __LINE__);
+	}
+
+	// copy the parameters data to the queue
+	err = clEnqueueWriteBuffer(commands, parameters, CL_TRUE, 0,
+			sizeof(double) * psize, p, 0, NULL, NULL);
+	if (err != CL_SUCCESS) {
+		fprintf(stderr, "%s:%d: cannot enqueue the matrix: %d\n",
+			__FILE__, __LINE__, err);
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "%s:%d: argument data copied\n",
+			__FILE__, __LINE__);
+	}
+
+	// set the kernel arguments
+	err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &parameters);
+	err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &output);
+	if (err != CL_SUCCESS) {
+		fprintf(stderr, "%s:%d: cannot set kernel arguments: %d\n",
+			__FILE__, __LINE__, err);
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "%s:%d: kernel arguments assigned\n",
+			__FILE__, __LINE__);
+	}
+
+	// compute a suitable work group size
+	size_t	global[2] = { width, height };
+	size_t	local[2] = { sx, sy };
+
+	// enqueue the kernel
+	err = clEnqueueNDRangeKernel(commands, kernel, 2, NULL, global, local,
+		0, NULL, NULL);
+	if (err) {
+		fprintf(stderr, "%s:%d: cannot enqueue the kernel: %d\n",
+			__FILE__, __LINE__, err);
+		return EXIT_FAILURE;
+	}
+	if (debug) {
+		fprintf(stderr, "%s:%d: kernel enqueued\n", __FILE__, __LINE__);
+	}
+
+	// read the result data from the queue. This method waits until the
+	// the kernel has finished
+	int	*o = (int *)malloc(sizeof(int) * width * height);
+	err = clEnqueueReadBuffer(commands, output, CL_TRUE, 0,
+		sizeof(int) * width * height, o, 0, NULL, NULL);
+	if (err != CL_SUCCESS) {
+		fprintf(stderr, "%s:%d: cannot read the data: %d\n",
+			__FILE__, __LINE__, err);
+		return EXIT_FAILURE;
+	}
+
+	// perform cleanup
+	if (parameters) {
+		clReleaseMemObject(parameters);
+	}
+	if (output) {
+		clReleaseMemObject(output);
+	}
+
+	// release all the objects
+	clReleaseProgram(program);
+	clReleaseKernel(kernel);
+	clReleaseCommandQueue(commands);
+	clReleaseContext(context);
+
+	return EXIT_SUCCESS;
+}
---
+++ code/julia/julia.cl
@@ -0,0 +1,76 @@
+/*
+ * gauss.cl -- opencl kernel implementing gauss algorithm
+ *
+ * (c) 2014 Prof Dr Andreas Mueller, Hochschule Rapperswil
+ */
+#pragma OPENCL EXTENSION cl_khr_fp64: enable
+
+#ifdef AMD
+#pragma OPENCL EXTENSION cl_amd_printf : enable
+#endif
+
+#ifdef INTEL
+#pragma OPENCL EXTENSION cl_intel_printf : enable
+#endif
+
+/**
+ * \brief Kernel for Julia set computation
+ *
+ * \param parameters	array containing parameters for the computation
+ *                      The parameters are:
+ *                      [0]: originx
+ *                      [1]: originy
+ *                      [2]: grid constant in x direction
+ *                      [3]: grid constant in y direction
+ *                      [4]: Julia parameter real part
+ *                      [5]: Julia parameter imaginary part
+ *                      [6]: number of attracting points
+ *                      [7]: attracting point[0] real part
+ *                      [8]: attricting point[1] imaginary part
+ *                      additional attricting points follow 
+ * \param output	output array width x height
+ *                      width = get_global_size(0)
+ *                      height = get_global_size(1)
+ */
+__kernel void	iterate(__global double *parameters, __global int *output) {
+	// compute the initial value for the iteration
+	__private double	originx = parameters[0];
+	__private double	originy = parameters[1];
+	__private double	hx = parameters[2];
+	__private double	hy = parameters[3];
+	__private double2	c;
+	c.x = parameters[4];
+	c.y = parameters[5];
+
+	__private double2	z;
+	z.x = get_global_id(0) * hx;
+	z.y = get_global_id(1) * hy;
+
+	// now perform forward iteration for 100 steps
+	__private int	i;
+	for (i = 0; i < 100; i++) {
+		__private double	x = z.x * z.x - z.y * z.y + c.x;
+		z.y = 2 * z.x * z.y + c.y;
+		z.x = x;
+	}
+
+	// find out the nearest point from the list
+	__private int	npoints = parameters[6];
+	__private int	closest_index = 0;
+	__private double	dmin = 100000;
+	for (i = 0; i < npoints; i++) {
+		__private double2	p;
+		p.x = parameters[2 * i];
+		p.y = parameters[2 * i + 1];
+		__private double	d = distance(p, z);
+		if (d < dmin) {
+			closest_index = i;
+			dmin = d;
+		}
+	}
+
+	// we have now found the index of the closest point, so lets return it
+	output[get_global_id(0) + get_global_id(1) * get_global_size(0)]
+		= closest_index;
+}
+
